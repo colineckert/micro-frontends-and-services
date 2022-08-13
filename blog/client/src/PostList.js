b525434604b60ba00c81a34cd0aeef7a6f19af71
colineckert <colineckert10@gmail.com>
2022-08-12 21:39:15 -0700

    Create comment create and list

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentList from './CommentList';
+import CommentCreate from './CommentCreate';
 
 const PostList = () => {
 	const [posts, setPosts] = useState({});
@@ -18,6 +20,8 @@ const PostList = () => {
 			<div className='card' style={{ width: '30%', marginBottom: '20px' }}key={post.id}>
 				<div className='card-body'>
 					<h3>{post.title}</h3>
+					<CommentList postId={post.id} />
+					<CommentCreate postId={post.id} />
 				</div>
 			</div>
 		)
