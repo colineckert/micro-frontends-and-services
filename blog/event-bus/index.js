8bc317a126dce01e6475cec5aad9e46f74c441cf
colineckert <colineckert10@gmail.com>
2022-08-20 13:35:55 -0700

    Handle comment moderation and updating

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -11,10 +11,12 @@ app.post('/events', (req, res) => {
 	axios.post('http://localhost:4000/events', event);
 	axios.post('http://localhost:4001/events', event);
 	axios.post('http://localhost:4002/events', event);
+	axios.post('http://localhost:4003/events', event);
 
 	res.send({ status: 'OK' });
 });
 
 app.listen(4005, () => {
 	console.log('Listenting on 4005');
-})
+});
+
--- blog/query/index.js
+++ blog/query/index.js
@@ -22,10 +22,20 @@ app.post('/events', (req, res) => {
 	}
 
 	if (type === 'CommentCreated') {
-		const { id, content, postId } = data;
+		const { id, content, postId, status } = data;
 
 		const post = posts[postId];
-		post.comments.push({ id, content });
+		post.comments.push({ id, content, status });
+	}
+
+	if (type === 'CommentUpdate') {
+		const { id, content, postId, status } = data;
+
+		const post = posts[postId];
+		const comment = post.comments.find(comment => comment.id === id);
+
+		comment.status = status;
+		comment.content = content;
 	}
 
 	res.send({});
