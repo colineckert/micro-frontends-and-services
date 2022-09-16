cff3a5c2ad3685168d03474340af7c382dd7ff8f
colineckert <colineckert10@gmail.com>
2022-09-15 21:57:00 -0700

    Update urls to new services

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -23,21 +23,21 @@ app.post('/posts', async (req, res) => {
     title,
   };
 
-	await axios.post('http://localhost:4005/events', {
-		type: 'PostCreated',
-		data: { id, title }
-	})
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  });
 
   res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
-	console.log('Received Event:', req.body.type);
+  console.log('Received Event:', req.body.type);
 
-	res.send({});
+  res.send({});
 });
 
 app.listen(4000, () => {
-	console.log('v55');
+  console.log('v55');
   console.log('Listening on 4000');
 });
