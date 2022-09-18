d5173d5fee904884f183c1cad7c6298fdccfe1be
colineckert <colineckert10@gmail.com>
2022-09-17 20:06:42 -0700

    Update urls

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -24,43 +24,43 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-	await axios.post('http://localhost:4005/events', {
-		type: 'CommentCreated',
-		data: {
-			id: commentId,
-			content,
-			postId: req.params.id,
-			status: 'pending'
-		}
-	})
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
 
   res.status(201).send(comments);
 });
 
 app.post('/events', async (req, res) => {
-	console.log('Received Event:', req.body.type);
+  console.log('Received Event:', req.body.type);
 
-	const { type, data } = req.body;
-	if (type === 'CommentModerated') {
-		const { postId, id, content, status } = data;
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { postId, id, content, status } = data;
 
-		const comments = commentsByPostId[postId];
-		const comment = comments.find(comment => comment.id === id);
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
 
-		comment.status = status;
+    comment.status = status;
 
-		await axios.post('http://localhost:4005/events', {
-			type: 'CommentUpdated',
-			data: {
-				id,
-				postId,
-				content,
-				status
-			}
-		})
-	}
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      },
+    });
+  }
 
-	res.send({});
+  res.send({});
 });
 
 app.listen(4001, () => {
