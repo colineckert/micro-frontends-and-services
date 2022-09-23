182225b4b8f326e5a4dd651fad369166025985a3
colineckert <colineckert10@gmail.com>
2022-09-22 21:11:17 -0700

    Setup client deployment:

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -3,15 +3,15 @@ import PostCreate from './PostCreate';
 import PostList from './PostList';
  
 const App = () => {
-  return (
-    <div className='container'>
-      <h1>Create Post</h1>
-      <PostCreate />
+	return (
+		<div className='container'>
+			<h1>Create Post</h1>
+			<PostCreate />
 			<hr />
 			<h1>Posts</h1>
 			<PostList />
-    </div>
-  )
+		</div>
+	)
 };
- 
+
 export default App;
