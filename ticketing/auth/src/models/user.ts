b3507a1bd80ee1a10282c70614db75be0cc612da
colineckert <colineckert10@gmail.com>
2022-10-22 06:44:31 -0700

    init user model

---
+++ ticketing/auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from 'mongoose';
+
+// An interface that describes the properties that are
+// reqquired to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
