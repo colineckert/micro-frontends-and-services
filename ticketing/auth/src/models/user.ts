c1bc303bc7e11e391fd55a27e885d0515ed659aa
colineckert <colineckert10@gmail.com>
2022-10-27 07:29:12 -0700

    Format returned schema from mongoose

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -21,16 +21,29 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        // delete ret.__v; //--> does the same as versionKey: false
+      },
+      versionKey: false,
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
