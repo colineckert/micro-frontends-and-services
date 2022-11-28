a619906b755be67dd2cb31e434051c822cc0abc5
colineckert <colineckert10@gmail.com>
2022-11-28 05:46:39 -0800

    redirect on successful signup

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
@@ -9,6 +9,7 @@ const Signup = () => {
     url: '/api/users/signup',
     method: 'post',
     body: { email, password },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async (event) => {
