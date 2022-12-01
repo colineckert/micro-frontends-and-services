cc0cb78c471c2483a67eba68e6472a3ec2162c53
colineckert <colineckert10@gmail.com>
2022-12-01 07:17:58 -0800

    pass headers to server request

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -6,17 +6,13 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server! make request in correct kubernetes namespace
     const { data } = await axios
       .get(
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-        {
-          headers: {
-            Host: 'ticketing.dev',
-          },
-        }
+        { headers: req.headers }
       )
       .catch((err) => {
         console.log(err.message);
