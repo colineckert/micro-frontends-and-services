b8959d9487da67844c75c49b160bf20b72697eaf
colineckert <colineckert10@gmail.com>
2022-12-01 07:14:52 -0800

    correct make server-side request

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,38 @@
-const Landing = () => {
-	return <h1>Landing page</h1>
-}
+import axios from 'axios';
 
-export default Landing;
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server! make request in correct kubernetes namespace
+    const { data } = await axios
+      .get(
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+        {
+          headers: {
+            Host: 'ticketing.dev',
+          },
+        }
+      )
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    return data;
+  } else {
+    // we are on the client! can use a base url of ''
+    const { data } = await axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err.message);
+    });
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
