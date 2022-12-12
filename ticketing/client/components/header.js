eec4d986fba9ecb5b3fe55b515e6aa3b425d28cf
colineckert <colineckert10@gmail.com>
2022-12-12 06:39:36 -0800

    Conditionally render nav items based on currentUser

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,15 +1,27 @@
 import Link from 'next/link';
 
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => (
+      <li key={href} className="nav-item">
+        <Link href={href} className="nav-link">
+          {label}
+        </Link>
+      </li>
+    ));
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link className="navbar-brand" href="/">
         GitTix
       </Link>
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? 'Sign out' : 'Sign in/up'}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
