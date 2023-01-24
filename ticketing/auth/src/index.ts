abdec4e80651f96a006c75c3df124ff600fc5fca
colineckert <colineckert10@gmail.com>
2023-01-24 06:40:22 -0800

    add MONGO_URI env vars

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -6,9 +6,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT _KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must bed defined');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.log(err);
