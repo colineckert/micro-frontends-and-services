7341b89cc7de0777285dd08ad808e8e3677e40df
colineckert <colineckert10@gmail.com>
2023-03-05 21:22:22 -0800

    Finish setup for publisher

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -6,4 +6,15 @@ const stan = nats.connect('ticketing', 'abc', {
 
 stan.on('connect', () => {
   console.log('Publisher connect to NATS');
-})
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  })
+});
+
