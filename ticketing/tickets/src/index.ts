5b6fa8d6ea4714fc929d12cc6944dbf6b1c94197
colineckert <colineckert10@gmail.com>
2023-03-17 20:25:17 -0700

    exit nats server on close

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -12,6 +12,14 @@ const start = async () => {
 
   try {
     await natsWrapper.connect('ticketing', '120397jk', 'http://nats-srv:4222');
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
