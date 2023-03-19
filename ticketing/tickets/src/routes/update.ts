f32ab0ab6158c9e83cbef98068170adf7c23238b
colineckert <colineckert10@gmail.com>
2023-03-19 16:10:26 -0700

    add publisher to update route

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from '@ce-tickets/common';
 import { Ticket } from '../models/tickets';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -37,6 +39,13 @@ router.put(
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
