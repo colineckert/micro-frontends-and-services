877feebc69cac0c9151e99a7cf0c021bccf0f7c8
colineckert <colineckert10@gmail.com>
2023-03-21 21:05:01 -0700

    mock publish function and add tests

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/tickets';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -72,4 +73,15 @@ it('creates a ticket with valid inputs', async () => {
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
   expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+  const title = 'test title';
+  await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+    title,
+    price: 20,
+  });
+  expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
