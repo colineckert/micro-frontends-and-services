0e2052d2f5bea75499e51b1a0e17e28e09488980
colineckert <colineckert10@gmail.com>
2023-08-24 06:25:03 -0700

    update mount for products

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,9 +1,22 @@
 import faker from 'faker';
 
-let products = '';
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+const mount = (el) => {
+  let products = '';
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// running products in dev in isolation
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+
+  // Assuming out container doesn't have an element with #dev-products
+  if (el) mount(el);
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// running products from within container
+export { mount };
