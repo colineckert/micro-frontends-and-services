24413a82b424e48f81b6781e7bbf3ee4cb65d739
colineckert <colineckert10@gmail.com>
2023-08-28 08:54:03 -0700

    create MarketingApp component

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <h1>Hi there!</h1>;
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
 }
